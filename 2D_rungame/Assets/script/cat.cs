ff7e001f3cfafb1b83073840462a7ac839fc13d7
g7752157 <54761463+g7752157@users.noreply.github.com>
2019-10-20 16:06:03 +0800

    動畫編輯

---
+++ 2D_rungame/Assets/script/cat.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+public class cat : MonoBehaviour
+{
+    #region 欄位
+    [Header("跳躍次數")][Range(1, 10)][Tooltip("連跳次數")]
+    public int JumpCount;
+    [Header("跳躍高度")]
+    public int Jump = 100;
+    [Header("跑速")]
+    [Range(1, 10)]
+    public float speed = 1.5f;
+    [Header("地面判定")]
+    public bool IsGround;
+    [Header("狗名")]
+    public string CatName = "cat";
+    public Transform CatTransform, cam;
+    #endregion
+    private void Start()
+    {
+     
+    }
+
+    private void Update()
+    {
+        MoveCat();
+        MoveCamera();
+    }
+    /// <summary>
+    /// move cat
+    /// </summary>
+    private void MoveCat()
+    {
+        //deltaTime= time passed per frame
+        CatTransform.Translate(speed*Time.deltaTime, 0, 0);
+
+    }
+
+    /// <summary>
+    /// move camera
+    /// </summary>
+    private void MoveCamera()
+    {
+        cam.position = new Vector3(CatTransform.position.x, cam.position.y, cam.position.z);
+
+    }
+    /// <summary>
+    /// jump
+    /// </summary>
+    public void CatJump()
+    {
+        print("jump");
+    }
+    /// <summary>
+    /// slide
+    /// </summary>
+    public void CatSlide()
+    {
+        print("slide");
+    }
+}
